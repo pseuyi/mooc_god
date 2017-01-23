3d92e93d847132f959b1afeb1711a510d8303a69
freda nada <fredasnada@gmail.com>
2017-01-23 11:27:09 -0500

    init

---
+++ setup.js
@@ -0,0 +1,84 @@
+setup
+
+git init
+npm init
+npm init y
+<scripts>
+	"start": "nodemon -e html,js,css ./path/to/your/app.js"
+
+<npm all>
+	npm install express body-parser morgan sequelize pg pg-hstore nunjucks --save
+	npm install nodemon --save-dev
+
+<express>
+	var express = require('express');
+	var app = express();
+	app.use(express.static('public'));
+	app.use('/static', express.static('public'));
+	app.use('/static', express.static(__dirname + '/public'));
+
+	<router>
+	var express = require('express');
+	var router = express.Router();
+	var models = require('../models');
+	var Promise = require('bluebird');
+		var Page = models.Page;
+		var User = models.User;
+	module.exports = router;
+
+<body-parser>
+	npm i body-parser
+	var bodyParser = require('body-parser');
+	// true uses qs lib, false uses querystring
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+
+<nunjucks>
+	npm i nunjucks
+	var nunjucks = require('nunjucks');
+	app.set('view engine', 'html');
+	app.engine('html', nunjucks.render);
+	nunjucks.configure('views', {noCache: true} ); //specify a view folder, turn off cache for dev purposes
+
+<mocha + supertest + npm test>
+	if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
+
+<path>
+	var path = require('path');
+	requiring path overrides links
+
+<sequelize>
+	npm install sequelize pg pg-hstore --save
+	// pg/pg-hstore necessary to direct sql orl to interact w postgresSQL
+	bluebird is a dependency
+	ex. var Promise = require('bluebird')
+	sequelize.ARRAY can only be used with postgres
+	//required for models
+	var Sequelize = require('sequelize');
+	var db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});//logging false will clean up tests
+
+<bluebird>
+	npm install bluebird
+	var Promise = require("bluebird");
+<fs>
+	built into node library
+	var fs = require('fs');
+
+<morgan>
+	var morgan = require('morgan')
+	app.use(morgan('combined'))
+	app.use(morgan('dev'))
+	// only log errors
+	app.use(morgan('combined', {
+	  skip: function (req, res) { return res.statusCode < 400 }
+	}))
+
+<error handling>
+app.use(function(err, req, res, next){
+	res.status(err.status || 500);
+	console.error(err);
+});
+
+*any files to gitignore?
+
+module.exports = app;
